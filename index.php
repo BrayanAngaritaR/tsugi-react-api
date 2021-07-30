8a1dce28166d3d8fc08dc7153b9b82142f7e4125
Charles Severance <csev@umich.edu>
2021-07-30 12:03:53 -0400

    Moreing toward cleaning up and tightening up.

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+
+require_once "../config.php";
+
+use \Tsugi\Core\LTIX;
+
+// No parameter means we require CONTEXT, USER, and LINK
+$LAUNCH = LTIX::requireData();
+
 $data = file_get_contents("tsugi/index.html");
-echo($data);
+$pos = strpos($data,"</head>");
+echo(substr($data,0,$pos));
+$OUTPUT->headerData();
+echo(substr($data,$pos));
+
+
--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -3,7 +3,10 @@ import { PageSection, Title } from '@patternfly/react-core';
 
 const Dashboard: React.FunctionComponent = () => (
   <PageSection>
-    <Title headingLevel="h1" size="lg">Dashboard Page Title!</Title>
+    <Title headingLevel="h1" size="lg">Tsugi React</Title>
+    <p>
+        Welcome { _TSUGI.user_displayname } from { _TSUGI.context_title }
+    </p>
   </PageSection>
 )
 
