7a38326a093e05e1d491ddc784d9cbee5600c740
Charles Severance <csev@umich.edu>
2021-07-30 11:35:19 -0400

    Include index.html

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-<!doctype html><html lang="en-US"><head><meta charset="utf-8"><title>Tsugi React</title><meta id="appName" name="application-name" content="Patternfly Seed"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" type="image/svg+xml" href="/images/favicon.png"><base href="/"><script defer="defer" src="http://localhost:8888/py4e/mod/patternfly-seed/tsugi/main.bundle.js"></script><link href="http://localhost:8888/py4e/mod/patternfly-seed/tsugi/main.css" rel="stylesheet"></head><body><noscript>Enabling JavaScript is required to run this app.</noscript><div id="root"></div></body></html>
+<?php
+$data = file_get_contents("tsugi/index.html");
+echo($data);
