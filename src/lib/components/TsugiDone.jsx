b28f69f74124c57135385f9bdc1f17d2f5d0081a
Charles Severance <csev@umich.edu>
2021-08-02 17:30:03 -0400

    Moar refactor

--- src/lib/components/TsugiDone.jsx
+++ src/lib/components/TsugiDone.jsx
@@ -5,21 +5,7 @@ import {
   PageHeaderTools,
 } from '@patternfly/react-core';
 
-
-function inIframe(props) {
-  var retval =  (window.location !== window.parent.location );
-  return ( retval );
-}
-
-
-// https://stackoverflow.com/questions/19761241/window-close-and-self-close-do-not-close-the-window-in-chrome
-// How to close a window even if we did not open it
-function window_close()
-{
-    window.close();
-    setTimeout(function(){ console.log("Attempting self.close"); self.close(); }, 1000);
-    setTimeout(function(){ console.log("Notifying the user."); alert(_TSUGI.window_close_message); open("about:blank", '_self').close(); }, 2000);
-}
+import { inIframe, window_close } from './tsugi.js';
 
 function TsugiDone() {
   function handleClick(e) {
